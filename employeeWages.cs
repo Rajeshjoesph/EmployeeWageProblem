78c1e1b7582c1095267ede4f371970d1bf467080
“rajeshjoshph” <rajeshkannan1999.rk@gmail.com>
2023-02-15 13:51:14 -0800

    Add: Employee Daily Wages Problem added

---
+++ employeeWages.cs
@@ -0,0 +1,28 @@
+﻿namespace EmployeeWages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck=random.Next(0,2);
+            int wagesPerHour = 20;
+            int workHour=0, empWages=0;
+            if(empCheck==FULL_TIME)
+            {
+                workHour = 8;
+
+            }
+            else
+            {
+                workHour = 0;
+            }
+            empWages = workHour * wagesPerHour;
+
+            Console.WriteLine("Employee Daily Wages: " + empWages);
+            
+            
+        }
+    }
+}
