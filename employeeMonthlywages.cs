da1e9b8b3948e820c9bcde2473a3fecd82cacec0
“rajeshjoshph” <rajeshkannan1999.rk@gmail.com>
2023-02-16 15:31:09 -0800

    Add: Monthly Employee Wages problem added

---
+++ employeeMonthlywages.cs
@@ -0,0 +1,42 @@
+﻿namespace employeeMonthlyWages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int FULL_TIME = 1;
+            const int PART_TIME = 2;
+            const int TOTAL_WORK_DAYS = 20;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            int wagesPerHour = 20;
+            int workHour = 0, empWages = 0, totwages = 0;
+            for (int day = 0; day < TOTAL_WORK_DAYS; day++)
+            {
+                switch (empCheck)
+                {
+
+                    case FULL_TIME:
+                        workHour = 8;
+                        break;
+                    case PART_TIME:
+                        workHour = 4;
+                        break;
+                    default:
+                        workHour = 0;
+                        break;
+                }
+
+                empWages = workHour * wagesPerHour;
+                totwages = totwages + empWages;
+
+
+            }
+            Console.WriteLine("Employee Daily Wages: " + empWages);
+            Console.WriteLine("Employee Total Wages: " + totwages);
+
+
+
+        }
+    }
+}
